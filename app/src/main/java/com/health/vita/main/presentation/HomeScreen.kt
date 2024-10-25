d026468ee6cf44c1e37efbe279cdc10bf48fc5a7
elkofix <holasoygermamwow@gmail.com>
2024-10-25 14:50:36 -0500

    feat: complete screen w/o logic

--- app/src/main/java/com/health/vita/main/presentation/HomeScreen.kt
+++ app/src/main/java/com/health/vita/main/presentation/HomeScreen.kt
@@ -1,14 +1,82 @@
 package com.health.vita.main.presentation
 
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavController
+import com.health.vita.R
+import com.health.vita.ui.components.main.CardWithTitle
+import com.health.vita.ui.components.main.ProfileCard
+import com.health.vita.ui.theme.VitaTheme
+
 
 @Composable
-fun HomeScreen(navController : NavController = rememberNavController()) {
-    Column {
+fun HomeScreen(navController: NavController = rememberNavController()) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        content = { innerPadding ->
+            Column(modifier = Modifier.padding(innerPadding)) {
 
+                ProfileCard()
+                Column(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(ScrollState(0))
+                    ,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CardWithTitle("Entrenamientos", R.drawable.main_sportcard){
+                        Text(
+                            text = "Proximamente",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.background,
+                            modifier = Modifier
+                                .padding(16.dp)
+                        )
+                    }
+                    CardWithTitle("Alimentacion",R.drawable.main_mealcard){
+                        Text(
+                            text = "Proximamente",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier
+                                .padding(16.dp)
+                        )
+                    }
+
+                    CardWithTitle("Entrenador IA", R.drawable.main_iacard){
+                        Text(
+                            text = "Proximamente",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.background,
+                            modifier = Modifier
+                                .padding(16.dp)
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun homePrev(){
+    VitaTheme {
+        HomeScreen()
     }
 }
--- app/src/main/java/com/health/vita/ui/components/main/ProfileCard.kt
+++ app/src/main/java/com/health/vita/ui/components/main/ProfileCard.kt
@@ -1,72 +1,94 @@
 package com.health.vita.ui.components.main
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.motionEventSpy
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.firebase.Timestamp
 import com.health.vita.R
+import com.health.vita.ui.theme.VitaTheme
+import java.text.SimpleDateFormat
+import java.util.Locale
+val dateFormat = SimpleDateFormat("MMM dd, yyyy", Locale("es"))
 
-@Preview
 @Composable
-fun ProfileCard() {
-    Row(
-        Modifier
-            .background(MaterialTheme.colorScheme.primary)
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
+fun ProfileCard(
+    name: String = "John",
+    date: Timestamp = Timestamp.now()
+) {
+    Column {
         Row(
             Modifier
-                .weight(2f)
-                .padding(horizontal = 12.dp),         verticalAlignment = Alignment.CenterVertically,
-        ){
-            CircularPhoto(photo = R.drawable.male_image, "profile picture", 78)
-            Column(Modifier.padding(horizontal = 12.dp)) {
-                Text(
-                    text = "Alejandro",
-                    style = MaterialTheme.typography.titleMedium,
+                .background(MaterialTheme.colorScheme.primary)
+                .fillMaxWidth()
+                .padding(top = 48.dp, bottom = 18.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                Modifier
+                    .weight(2f)
+                    .padding(horizontal = 12.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                CircularPhoto(photo = R.drawable.male_image, "profile picture", 78)
+                Column(Modifier.padding(horizontal = 12.dp)) {
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.background
+                    )
+                    Text(
+                        text = dateFormat.format(date.toDate()),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.background
+                    )
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.weight(1f)
+            ) {
+                CircularIconOutlinedIconButton(
+                    icon = R.drawable.baseline_notifications_24,
                     color = MaterialTheme.colorScheme.background
                 )
-                Text(
-                    text = "Jun 25, 2024",
-                    style = MaterialTheme.typography.titleMedium,
+                CircularIconOutlinedIconButton(
+                    icon = R.drawable.baseline_settings_24,
                     color = MaterialTheme.colorScheme.background
                 )
             }
-        }
-        Row(verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.weight(1f)
-        ){
-            CircularIconOutlinedIconButton(
-                icon = R.drawable.baseline_notifications_24,
-                color = MaterialTheme.colorScheme.background
-            )
-            CircularIconOutlinedIconButton(
-                icon = R.drawable.baseline_settings_24,
-                color = MaterialTheme.colorScheme.background
-            )
-        }
 
+        }
+        Box(
+            modifier = Modifier
+                .height(30.dp)
+                .fillMaxWidth()
+                .background(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(bottomStart = 30.dp, bottomEnd = 30.dp)
+                )
+        )
     }
 }
 
+@Preview(showBackground = false)
+@Composable
+fun PrevProfCard(){
+    VitaTheme {
+        ProfileCard()
+    }
+}
