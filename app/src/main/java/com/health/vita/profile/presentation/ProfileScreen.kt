c8be17b5b7a6142cc43953799cf4dec7f20bb6df
Ricardo965 <ricardiox2000@gmail.com>
2024-10-25 13:36:44 -0500

    hotfix: Calories aprox

--- app/src/main/java/com/health/vita/profile/presentation/ProfileScreen.kt
+++ app/src/main/java/com/health/vita/profile/presentation/ProfileScreen.kt
@@ -136,7 +136,7 @@ fun ProfileScreen(navController: NavController = rememberNavController(), profil
 
                 UserInfoCard(
                     label = "Ingesta diaria de kcal",
-                    value = "${calculateCalc()}",
+                    value = "${calculateCalc().toInt()}",
                     modifier = Modifier.fillMaxWidth()
                 )
             }
