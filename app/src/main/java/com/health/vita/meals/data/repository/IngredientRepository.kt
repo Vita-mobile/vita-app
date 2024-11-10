4adf0be420129ba52926af02f5fd5dbb1edd5277
Ricardo965 <ricardiox2000@gmail.com>
2024-11-10 08:31:47 -0500

    fix: ingredient repo fixed import

--- app/src/main/java/com/health/vita/meals/data/repository/IngredientRepository.kt
+++ app/src/main/java/com/health/vita/meals/data/repository/IngredientRepository.kt
@@ -1,6 +1,7 @@
 package com.health.vita.meals.data.repository;
 
 import com.health.vita.meals.data.data_source.IngredientService
+import com.health.vita.meals.data.data_source.IngredientServiceImpl
 import com.health.vita.meals.domain.model.Ingredient
 
 
@@ -9,7 +10,7 @@ interface IngredientRepository {
 }
 
 class IngredientRepositoryImpl(
-    private val ingredientService: IngredientService = IngredientServiceImp()
+    private val ingredientService: IngredientService = IngredientServiceImpl()
 ) : IngredientRepository{
     override suspend fun getIngredients(): List<Ingredient?> {
         return ingredientService.getIngredients()
